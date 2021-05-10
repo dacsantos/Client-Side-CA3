61bb3a2eac0db81274c877151468a1de346935f8
dacsantos <72028806+dacsantos@users.noreply.github.com>
2021-05-10 22:49:48 +0100

    second day commits

--- js/script.js
+++ js/script.js
@@ -1,14 +1,72 @@
-var total_items = 4;
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+var total_items = 13;
 
 function CalculateItemsValue(){
     var total = 0;
 
     for (let i = 1; i <= total_items; i++) {
        itemID =document.getElementById("qtt" + i);
-       total = total + parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price"));
+       total = total + itemID.value * itemID.getAttribute("data-price");
         
     }
-    document.getElementById('Total').innerHTML = "€" + total;
+    document.getElementById('Total').innerHTML = "€" + total.toFixed(2);
 }
 
 document.querySelectorAll('[id^="qtt"]').forEach(item => {
