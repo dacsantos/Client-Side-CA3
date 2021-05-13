5feb588b6e2cc30fdf261895755357880e075752
dacsantos <72028806+dacsantos@users.noreply.github.com>
2021-05-14 00:29:41 +0100

    fourth day commits

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
+
+// Dish images darken on hover
+const dishes = document.querySelectorAll(".dish-wrapper");
+
+dishes.forEach((dish) => {
+  dish.addEventListener("mouseover", () => {
+    dish.childNodes[1].classList.add("img-darken");
+  });
+  dish.addEventListener("mouseout", () => {
+    dish.childNodes[1].classList.remove("img-darken");
+  });
+});
+
+
 //Get Btn And Create Function
-//document.getElementById('myBtn').addEventListener('click', getData);
+document.getElementById("myBtn").addEventListener("click", getData);
 
 function getData() {
-    // console.log('test');
+  // console.log('test');
 
-    //Get API
-    fetch('https://randomuser.me/api/?results=5&nat=br,ie')
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data);
+  //Get API
+  fetch("https://randomuser.me/api/?results=5&nat=br,ie")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
 
-            let author = data.results;
-            // console.log(author);
+      let author = data.results;
+      // console.log(author);
 
-            //Get Data Value
-            let output = "";
+      //Get Data Value
+      let output = "";
 
-            //Get Data Loop Through
-            author.forEach(function (lists) {
-                output += `
+      //Get Data Loop Through
+      author.forEach(function (lists) {
+        output += `
                 
                         <img src="${lists.picture.medium}" class="img-customer">
                         <ul class="list-customers">
@@ -30,27 +44,9 @@ function getData() {
                         </ul>
                    
                 `;
-            });
+      });
 
-            //Show On Our Screen All Data
-            document.getElementById('output').innerHTML = output;
-
-        });
-};
-
-// Dish images darken on hover
-const dishes = document.querySelectorAll('.dish-wrapper')
-
-dishes.forEach(dish =>{
-
-    dish.addEventListener('mouseover', () =>{
-
-        dish.childNodes[1].classList.add('img-darken');
-
-    })
-    dish.addEventListener('mouseout', () =>{
-
-        dish.childNodes[1].classList.remove('img-darken');
-
-    })
-})
+      //Show On Our Screen All Data
+      document.getElementById("output").innerHTML = output;
+    });
+}
--- js/menuCalc.js
+++ js/menuCalc.js
@@ -1,103 +1,95 @@
-
-
 //Menu Table
 
 var total_items = 13;
 
-function CalculateItemsValue(){
-    var total = 0;
-    var totalStarters = 0;
-    var totalMain = 0;
-    var totalDesserts = 0;
-    var totalDrinks = 0;
+function CalculateItemsValue() {
+  var total = 0;
+  var totalStarters = 0;
+  var totalMain = 0;
+  var totalDesserts = 0;
+  var totalDrinks = 0;
 
+  for (let i = 1; i <= total_items; i++) {
+    itemID = document.getElementById("qtt" + i);
+    total = total + itemID.value * itemID.getAttribute("data-price");
+  }
+  document.getElementById("Total").innerHTML = "€" + total.toFixed(2);
+  document.getElementById("TotalClient").innerHTML = "€" + total.toFixed(2);
 
-    for (let i = 1; i <= total_items; i++) {
-
-       itemID =document.getElementById("qtt" + i);
-       total = total + itemID.value * itemID.getAttribute("data-price");
-        
+  for (let i = 1; i <= 4; i++) {
+    if (i != 2) {
+      itemID = document.getElementById("qtt" + i);
+      totalStarters =
+        totalStarters + itemID.value * itemID.getAttribute("data-price");
     }
-    document.getElementById('Total').innerHTML = "€" + total.toFixed(2);
-    document.getElementById('TotalClient').innerHTML = "€" + total.toFixed(2);
+  }
 
-    for (let i = 1; i <= 4; i++){
-        if(i!=2){
-        itemID = document.getElementById("qtt"+i);
-        totalStarters = totalStarters + itemID.value * itemID.getAttribute("data-price");
-        }
+  document.getElementById("totalStarters").innerHTML =
+    "€" + totalStarters.toFixed(2);
+
+  for (let i = 2; i <= 7; i++) {
+    if (i != 3 && i != 4) {
+      itemID = document.getElementById("qtt" + i);
+      totalMain = totalMain + itemID.value * itemID.getAttribute("data-price");
     }
-    
-    document.getElementById('totalStarters').innerHTML = "€" + totalStarters.toFixed(2);
+  }
 
-    for (let i = 2; i <= 7; i++){
-        if(i!=3 && i!=4){
-        itemID = document.getElementById("qtt"+i);
-        totalMain = totalMain + itemID.value * itemID.getAttribute("data-price");
-        }
-    }
-    
-    document.getElementById('totalMain').innerHTML = "€" + totalMain.toFixed(2);
+  document.getElementById("totalMain").innerHTML = "€" + totalMain.toFixed(2);
 
-    for (let i = 8; i <= 10; i++) {
+  for (let i = 8; i <= 10; i++) {
+    itemID = document.getElementById("qtt" + i);
+    totalDesserts =
+      totalDesserts + itemID.value * itemID.getAttribute("data-price");
+  }
+  document.getElementById("totalDesserts").innerHTML =
+    "€" + totalDesserts.toFixed(2);
 
-        itemID = document.getElementById("qtt" + i);
-        totalDesserts = totalDesserts + itemID.value * itemID.getAttribute("data-price");
-         
-     }
-     document.getElementById('totalDesserts').innerHTML = "€" + totalDesserts.toFixed(2);
+  for (let i = 11; i <= 13; i++) {
+    itemID = document.getElementById("qtt" + i);
+    totalDrinks =
+      totalDrinks + itemID.value * itemID.getAttribute("data-price");
+  }
+  document.getElementById("totalDrinks").innerHTML =
+    "€" + totalDrinks.toFixed(2);
+}
 
-    for (let i = 11; i <= 13; i++) {
-
-        itemID = document.getElementById("qtt" + i);
-        totalDrinks = totalDrinks + itemID.value * itemID.getAttribute("data-price");
-         
-     }
-     document.getElementById('totalDrinks').innerHTML = "€" + totalDrinks.toFixed(2);
-
-    }
-
-document.querySelectorAll('[id^="qtt"]').forEach(item => {
-    item.addEventListener('change', CalculateItemsValue);
+document.querySelectorAll('[id^="qtt"]').forEach((item) => {
+  item.addEventListener("change", CalculateItemsValue);
 });
-
 
 //Calculate Vegetarian
 
-function CalculateVegetarian(){
-    var totalVeg = 0;
+function CalculateVegetarian() {
+  var totalVeg = 0;
 
-  
-       itemID1 = document.getElementById("qtt1");
-       itemID2 = document.getElementById("qtt2");
-       totalVeg = totalVeg + itemID1.value * itemID1.getAttribute("data-price") + itemID2.value * itemID2.getAttribute("data-price");
-        
- 
-    document.getElementById('vegetarian').innerHTML = "€" + totalVeg.toFixed(2);
+  itemID1 = document.getElementById("qtt1");
+  itemID2 = document.getElementById("qtt2");
+  totalVeg =
+    totalVeg +
+    itemID1.value * itemID1.getAttribute("data-price") +
+    itemID2.value * itemID2.getAttribute("data-price");
+
+  document.getElementById("vegetarian").innerHTML = "€" + totalVeg.toFixed(2);
 }
 
-document.querySelectorAll('[id^="qtt"]').forEach(item => {
-    item.addEventListener('change', CalculateVegetarian);
+document.querySelectorAll('[id^="qtt"]').forEach((item) => {
+  item.addEventListener("change", CalculateVegetarian);
 });
 
 //Calculate Non-Vegetarian
 
+function CalculateNonVeg() {
+  var totalNonVeg = 0;
 
-function CalculateNonVeg(){
-    var totalNonVeg = 0;
-
-    for (let i = 3; i <= 7; i++) {
-       itemID =document.getElementById("qtt" + i);
-       totalNonVeg = totalNonVeg + itemID.value * itemID.getAttribute("data-price");
-        
-    }
-    document.getElementById('non-vegetarian').innerHTML = "€" + totalNonVeg.toFixed(2);
+  for (let i = 3; i <= 7; i++) {
+    itemID = document.getElementById("qtt" + i);
+    totalNonVeg =
+      totalNonVeg + itemID.value * itemID.getAttribute("data-price");
+  }
+  document.getElementById("non-vegetarian").innerHTML =
+    "€" + totalNonVeg.toFixed(2);
 }
 
-document.querySelectorAll('[id^="qtt"]').forEach(item => {
-    item.addEventListener('change', CalculateNonVeg);
+document.querySelectorAll('[id^="qtt"]').forEach((item) => {
+  item.addEventListener("change", CalculateNonVeg);
 });
-
-
-
-
